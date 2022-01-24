67b139267d80d044d2beb8f04d5074d7a621cb29
root <root@LAPTOP-4MQ15EMS.localdomain>
2022-01-24 10:05:09 -0500

    js

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,9 @@
+function welcome(firstName, lastName){
+    let fullName = firstName + " " +  lastName;
+
+    function displayFullName(){
+        alert("welcome" + " " + fullName);
+    }
+
+    displayFullName();
+}
---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+function welcomeMessage(fullName){
+    alert("welcome" + " " + fullName);
+}
+
+let guillaume = welcomeMessage("guillaume");
+
+let alex = welcomeMessage("alex");
+
+let fred = welcomeMessage("fred");
